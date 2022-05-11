24d97a8520872c7caa130932df71e4058641c4cc
MythXLegend <85981133+MythXLegend@users.noreply.github.com>
2022-05-12 00:17:40 +0530

    Update data.js

--- data.js
+++ data.js
@@ -2,13 +2,13 @@ module.exports = {
 
     mongoLink:'',//database link
     
-    bot_token:'1956917944:AAHQdSXqyGKFnbf4FbO9IBAoKDca6kSUVn0',//bot token
+    bot_token:'5342266699:AAFI_vWNxSvlPcL0z9bqlrxubeMzu5TRrmE',//bot token
     
-    bot_name:'New1trybot',//bot username without @
+    bot_name:'PikaCryptoAirdropBot',//bot username without @
     
-    bot_admin:65555,// admin telegram id
+    bot_admin:MythXIegend,// admin telegram id
     
-    channelsList : ['@FortniteAirdrop'],// channels list
+    channelsList : ['@Airdropws'],// channels list
     
     reffer_bonus:1000,// refferal bonus amount
     
@@ -16,7 +16,7 @@ module.exports = {
     
     daily_bonus:0.001,// daily bonus amount
     
-    currency:'', //bot cuurency
+    currency:'PIKA', //bot cuurency
     
     cb_api_key:'',//coinbase api key
     
@@ -24,6 +24,6 @@ module.exports = {
     
     cb_account_id:'',// counbase account id for the bot currency
     
-    payment_channel:'@payoutproof12'// payment channel username
+    payment_channel:'@payoutSHOUT'// payment channel username
     
     }
